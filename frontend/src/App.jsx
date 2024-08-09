aaf7ba00a1c3c475a988272cd2ad19cedab65ac9
Shivang Medheakar <69140290+shivangmedhekar@users.noreply.github.com>
2024-08-09 21:22:50 +0530

    Sprint 2 ready for development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
+import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import SignIn from "./Components/Auth/SignIn";
 import SignUp from "./Components/Auth/SignUp";
 import FAQ from "./Components/FAQ/FAQ";
+import Article from "./Components/Article/Article";
+import Settings from "./Components/Profile/Settings/Settings";
+import NotFound from "./Components/NotFound/NotFound";
 import './App.css'
 
 function App() {
@@ -13,9 +17,13 @@ function App() {
       <NavBar />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/faq" element={<FAQ />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="article" element={<Article />}/>
+          <Route path="settings" element={<Settings />}/>
         </Routes>
       </BrowserRouter>
       <Footer />
