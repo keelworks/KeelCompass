fba04a2bb07ba41f145d600d0756ce3d899e8ff2
Dharma Thanishq Nimmala <nimmala.d@northeastern.edu>
2024-08-27 19:42:39 -0400

    updating article page

--- frontend/src/Components/Article/Article.jsx
+++ frontend/src/Components/Article/Article.jsx
@@ -26,16 +26,19 @@ const Article = () => {
           <div className="flex mt-8 mb-8">
             <div className="w-1/4 bg-white p-4">
               <ul className="space-y-4 text-right">
-                <li className="text-black font-semibold hover:text-blue-500 cursor-pointer transition duration-200">
+                <li className="text-grey-700 hover:text-black cursor-pointer transition duration-200 text-sm underline">
+                  Articles in this category
+                </li>
+                <li className="text-grey-700 hover:text-black cursor-pointer transition duration-200 text-sm underline">
                   Home
                 </li>
-                <li className="text-black font-semibold hover:text-blue-500 cursor-pointer transition duration-200">
+                <li className="text-grey-700 hover:text-black cursor-pointer transition duration-200 text-sm underline">
                   Articles
                 </li>
-                <li className="text-black font-semibold hover:text-blue-500 cursor-pointer transition duration-200">
+                <li className="text-grey-700 hover:text-black cursor-pointer transition duration-200 text-sm underline">
                   About
                 </li>
-                <li className="text-black font-semibold hover:text-blue-500 cursor-pointer transition duration-200">
+                <li className="text-grey-700 hover:text-black cursor-pointer transition duration-200 text-sm underline">
                   Contact
                 </li>
               </ul>
@@ -43,7 +46,7 @@ const Article = () => {
 
             {/* Right Content */}
             <div className="w-2/3 bg-white rounded-lg p-6 ml-4">
-              <h2 className="text-2xl font-bold text-black mb-2">
+              <h2 className="text-4xl font-bold text-black mb-5">
                 Article Title
               </h2>
               <p className="text-sm text-gray-500 mb-4">by Author Name</p>
@@ -51,6 +54,13 @@ const Article = () => {
                 This is where the body of the article will be displayed. You can
                 include multiple paragraphs, images, and other content elements
                 here to convey your message effectively.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                
               </p>
             </div>
           </div>
