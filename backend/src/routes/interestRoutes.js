8d59c8dc3613ff98cbbce58bea4c2434f402816c
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2024-11-30 18:05:37 -0500

    added auth middleware for interests endpoint

--- backend/src/routes/interestRoutes.js
+++ backend/src/routes/interestRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const interestController = require('../controllers/interestController');
-// const authMiddleware = require('../middleware/authMiddleware');
+const authenticator = require("../middlewares/authMiddleware");
 
 // Protected route to get user interests
-router.get('/', interestController.getUserInterests);
+router.get('/', authenticator, interestController.getUserInterests);
 
 // Protected route to save a new interest
-router.post('/', interestController.saveInterest);
+router.post('/', authenticator, interestController.saveInterest);
 
 // Protected route to delete an interest by ID
-router.delete('/:id', interestController.deleteInterest);
+router.delete('/:id', authenticator, interestController.deleteInterest);
 
 module.exports = router;
