b35905cbce79924f3923bdea740a9ac314321da8
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-12-04 18:40:53 -0500

    backdrop updated

--- src/components/Dashboard/Buttons/LikeButton.tsx
+++ src/components/Dashboard/Buttons/LikeButton.tsx
@@ -7,7 +7,7 @@ const LikeButton = () => {
     return (
       <button  className= "flex items-center space-x-1 text-gray-700 hover:text-blue-500">
       <img
-        src= "/images/like-svgrepo-com.svg" // Correct path to the image
+        src= "/images/like-svgrepo-com.svg" 
         alt="like"
         onClick={() => setLikes(likes + 1)}
         className="w-3 h-3 cursor-pointer"
