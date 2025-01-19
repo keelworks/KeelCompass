e9dc92d507d2f0adb53ccce0ef0bf7a09306ec74
Shivang Medheakar <69140290+shivangmedhekar@users.noreply.github.com>
2025-01-19 18:20:37 -0500

    Updated Design

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import DashboardWrapper from "../components/Dashboard/DashboardWrapper";
 import PostsSection from "../components/Dashboard/Sections/PostsSection";
 import CommunityUpdatesSection from "../components/Dashboard/Sections/CommunityUpdatesSection";
@@ -6,18 +5,37 @@ import MyInterestsSection from "../components/Dashboard/Sections/MyInterestsSect
 
 const Dashboard = () => {
   return (
+   
     <DashboardWrapper>
-      {/* Left Column: Posts and Community Updates */}
-      <div className="col-span-2 space-y-8">
+      {/* Left Column */}
+      <div className="col-span-2 flex flex-col">
         <PostsSection />
-        <CommunityUpdatesSection />
+        {/* Wrap CommunityUpdatesSection in a container */}
+        <div className="mt-6 flex-grow">
+          <CommunityUpdatesSection />
+        </div>
       </div>
 
-      {/* Right Column: My Interests */}
-      <div className="col-span-1">
-        <MyInterestsSection />
+      {/* Right Column */}
+      <div className="col-span-1 flex flex-col">
+        {/* Ask Question Button */}
+        <div className="mb-8">
+          <button className= "bg-custom-gradient text-white font-medium w-full py-3 rounded-lg hover:bg-teal-500/90 transition duration-200">
+            Ask Question
+          </button>
+        </div>
+
+        {/* Wrap MyInterestsSection in a container */}
+        <div className="flex-grow">
+          <MyInterestsSection/>
+        </div>
       </div>
     </DashboardWrapper>
+   
+
+    
+    
+      
   );
 };
 
